3818a79853685602299c4394495a5e1b6cb1c0ab
qxbash <xuereb@quinx.net>
2026-09-19 13:24:12 -0500

    Completed in-class stack methods with a short test in Main.java

--- InClassNotes/src/main/java/week3/ArrayBoundedStack.java
+++ InClassNotes/src/main/java/week3/ArrayBoundedStack.java
@@ -8,34 +8,49 @@ public class ArrayBoundedStack<T> implements StackInterface<T> {
     public ArrayBoundedStack(){
         elements = (T[]) new Object[DEFAULTCAP];
     }
+    
     public ArrayBoundedStack(int maxSize){
         elements = (T[]) new Object[maxSize];
     }
+    
     public boolean isEmpty()
     {
         //TODO: how do we determine if the stack is empty? Implement this method
-        return false;
-    }
+        
+        return topIndex == -1; 
+    } // The stack cannot be negative, if it is negative, it is empty.
 
     public boolean isFull()
     {
         //TODO: How do we determine if the stack is full? Implement this method
-        return false;
+        return topIndex + 1 == elements.length; // Checks whether the the stack is full by checking the size of the elements inside
     }
     
     public void push(T element)
     {
        // TODO: How do we add an element to the stack? Implement this method
-    }
+       if (!isFull()) {
+         // If it is not full it adds an additional element to the stack.
+        topIndex ++;
+        elements[topIndex] = element;
+       }
+    } 
     
     public void pop()
     {
         // How do we remove an element from the stack? Implement this method
+        if (!isEmpty()) { 
+        topIndex --;
+        }
     }
     
     public T top()
     {
+        if (isEmpty()){
+            return null;
+        }
       // How do we return the top element of the stack without removing it? Implement this method
-      return null;   
+      // Returns nothing if empty, or it will return the latest element in the index.
+      return elements[topIndex];   
     }
 }
---
+++ InClassNotes/src/main/java/week3/Main.java
@@ -0,0 +1,18 @@
+package week3;
+import week1.*;
+
+public class Main {
+    public static void main(String args[]){
+        ArrayBoundedStack<Car> carStack = new ArrayBoundedStack<>();
+        ArrayBoundedStack<Vehical> vehicleStack = new ArrayBoundedStack<>();
+        ArrayBoundedStack<Integer> numberStack = new ArrayBoundedStack<>();
+
+        // Test with numbers first
+        System.out.println("Testing:");
+        numberStack.push(1);
+        numberStack.push(2);
+        numberStack.push(3);
+        
+       
+    }
+}
